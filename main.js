f552777efe7973107e01e7716ef4df9574b956b1
Ahlem Ahmed <ahmedahlem3@gmail.com>
2021-04-20 10:57:14 +0200

    middleware modified

--- main.js
+++ main.js
@@ -1,50 +1,38 @@
-const { static } = require("express")
-const express=require("express")
-const app=express()
+const { static } = require("express");
+const express = require("express");
+const app = express();
 
-const logger=(req,res,next)=>{
-    next()
-}
-// app.use(static("Public"))
-app.get("/",logger,(req,res)=>{
+const logger = (req, res, next) => {
     var d = new Date();
     var n = d.getHours();
     var day = d.getDay();
-    if ((n>=9 && n<=17) && (day != 6 || day !=7)) {
-        res.sendFile(__dirname+"/Public/Home.html")
-    } else {
-        res.sendFile(__dirname+"/Public/Close.html")
-    }
-})
+    if (n >= 9 && n <= 17 && (day != 6 || day != 7)){
+   return next();
+} 
+  return res.sendFile(__dirname + "/Public/Close.html")
+};
+app.use(logger)
+app.get("/", (req, res) => {
+ 
+    res.sendFile(__dirname + "/Public/Home.html");
+  
+});
 
-app.get("/Contact",logger,(req,res)=>{
-    var d = new Date();
-    var n = d.getHours();
-    var day = d.getDay();
-    if ((n>=9 && n<=17) && (day != 6 || day !=7)) {
-    res.sendFile(__dirname+"/Public/Contact.html")
-} else {
-    res.sendFile(__dirname+"/Public/Close.html")
-}
-})
-app.get("/Services",logger,(req,res)=>{
-    var d = new Date();
-    var n = d.getHours();
-    var day = d.getDay();
-    if ((n>=9 && n<=17) && (day != 6 || day !=7)) {
-    res.sendFile(__dirname+"/Public/Services.html")
-} else {
-    res.sendFile(__dirname+"/Public/Close.html")
-}
-})
-app.get("/Css/style.css",logger,(req,res)=>{
-    res.sendFile(__dirname+"/Public/Css/style.css")
-})
+app.get("/Contact", (req, res) => {
+  
+    res.sendFile(__dirname + "/Public/Contact.html");
+ 
+});
+app.get("/Services", (req, res) => {
+  
+    res.sendFile(__dirname + "/Public/Services.html");
+  
+});
+app.get("/Css/style.css", (req, res) => {
+  res.sendFile(__dirname + "/Public/Css/style.css");
+});
 
-
-
-
-const port=5100
-app.listen(port,()=>{
-console.log(`server running on port ${port}`)
-})
+const port = 5100;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
